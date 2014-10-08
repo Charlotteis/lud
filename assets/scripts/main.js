1a14ba59750584eacbbe34f7c22c699bb135034b
Charlotte Spencer <charlottelaspencer@gmail.com>
2014-10-09 00:17:03 +0100

    Added footer, header and javascript interactivity to show and hide table contents

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,30 @@
-console.log("Hello");
+// Grab each data entry (Date, Train Number, Duty Number)
+var dataTitles = document.getElementsByClassName("data-title");
+// Grab all the paragraphs accompanying each data entry
+var dataContent = document.getElementsByClassName("data-content");
+
+// When you click each data-entry, reveal the corresponding data-content or hide if already showing
+function toggleParagraph(event) {
+    // Using event allows you to ge the dom element from the context of the click!
+
+    var elementToShow = event.srcElement.parentNode.nextElementSibling;
+    if (elementToShow.style.display === "table-row") {
+        elementToShow.style.display = "none";
+        elementToShow.style.visibility = "hidden";
+    } else {
+        elementToShow.style.display = "table-row";
+        elementToShow.style.visibility = "visible";
+    }
+}
+
+// When you click each data-entry, reveal the corresponding data-content
+for (var i = 0; i < dataTitles.length; i++){
+    dataTitles[i].addEventListener("click", toggleParagraph, false);
+}
+
+
+// // On page load, hide the paragraph under each data entry
+for (var i = 0; i < dataContent.length; i++ ) {
+    dataContent[i].style.display = "none";
+    dataContent[i].style.visibility = "hidden";
+}
