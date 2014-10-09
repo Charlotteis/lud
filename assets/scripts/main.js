b1362c2e759539d1a2a2e995e049c8dbc57f28ad
Charlotte Spencer <charlottelaspencer@gmail.com>
2014-10-09 13:23:41 +0100

    Tidied up hide/show elements JS into functions

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,6 +3,21 @@ var dataTitles = document.getElementsByClassName("data-title");
 // Grab all the paragraphs accompanying each data entry
 var dataContent = document.getElementsByClassName("data-content");
 
+// Loops over an array of elements (most likely a classList) and hides them
+function hideElements(elements) {
+    for (var i = 0; i < elements.length; i++ ) {
+        elements[i].style.display = "none";
+        elements[i].style.visibility = "hidden";
+    }
+}
+
+// Loops over an array of elements and adds an event handler
+function addHandlerToArray(elements, fnc, handler) {
+    for (var i = 0; i < elements.length; i++){
+        elements[i].addEventListener(handler, fnc, false);
+    }
+}
+
 // When you click each data-entry, reveal the corresponding data-content or hide if already showing
 function toggleParagraph(event) {
     // Using event allows you to ge the dom element from the context of the click!
@@ -17,14 +32,7 @@ function toggleParagraph(event) {
     }
 }
 
-// When you click each data-entry, reveal the corresponding data-content
-for (var i = 0; i < dataTitles.length; i++){
-    dataTitles[i].addEventListener("click", toggleParagraph, false);
-}
-
-
-// // On page load, hide the paragraph under each data entry
-for (var i = 0; i < dataContent.length; i++ ) {
-    dataContent[i].style.display = "none";
-    dataContent[i].style.visibility = "hidden";
-}
+// On page load, hide the paragraph under each data entry
+hideElements(dataContent);
+// Add the "click" event to all data-titles, firing the toggleParagraph function
+addHandlerToArray(dataTitles, toggleParagraph, "click");
